60a1a86929bfdc5c972c026c395ec0bd524ab706
Thierry Michel Philippe Kleist <thierry@kle.ist>
2026-02-22 19:03:22 +0100

    refactor(comment): wire up provider interface
    Replace direct client/endpoints/git calls with provider abstraction.
    Update tests to use normalized note shapes (string author/resolved_by).
    Add plenary stubs to unit_helper for busted compatibility.

--- tests/unit_helper.lua
+++ tests/unit_helper.lua
@@ -1,1 +1,24 @@
 package.path = "./lua/?.lua;./lua/?/init.lua;" .. package.path
+
+-- Stub plenary.curl to avoid LuaRocks dependency
+package.preload["plenary.curl"] = function()
+  return {
+    get = function() end,
+    post = function() end,
+    patch = function() end,
+    delete = function() end,
+  }
+end
+
+-- Stub plenary.async and plenary.async.util
+package.preload["plenary.async"] = function()
+  return {
+    run = function() end,
+  }
+end
+
+package.preload["plenary.async.util"] = function()
+  return {
+    scheduler = function() end,
+  }
+end
